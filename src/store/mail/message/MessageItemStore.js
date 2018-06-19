ec17028b09d6d409709df89bc273a3ce6eccf37d
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2018-06-19 12:41:52 +0200

    - enhancement: removed sorting-related functionality from MessageItemStore (see conjoon/app-cn_mail#16)

--- src/store/mail/message/MessageItemStore.js
+++ src/store/mail/message/MessageItemStore.js
@@ -1,10 +1,10 @@
 /**
  * conjoon
- * (c) 2007-2017 conjoon.org
+ * (c) 2007-2018 conjoon.org
  * licensing@conjoon.org
  *
  * app-cn_mail
- * Copyright (C) 2017 Thorsten Suckow-Homberg/conjoon.org
+ * Copyright (C) 2018 Thorsten Suckow-Homberg/conjoon.org
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -67,152 +67,8 @@ Ext.define('conjoon.cn_mail.store.mail.message.MessageItemStore', {
         }
 
         return me.callParent(arguments);
-    },
-
-
-    /**
-     * (Local sort only) Inserts the passed Record into the Store at the index where it
-     * should go based on the current sort information.
-     *
-     * @param {Ext.data.Record} record
-     */
-    addSorted : function(record) {
-        var me         = this,
-            data       = me.getData(),
-            index      = me.findInsertIndexInPagesForRecord(record),
-            storeIndex = 0,
-            page, pos, values;
-
-        if (index === null) {
-            return null;
-        }
-
-        page   = index[0];
-        pos    = index[1];
-        values = data.map[page].value;
-
-        Ext.Array.splice(values, pos, 0, record);
-        Ext.Array.splice(values, values.length - 1, 1);
-
-
-        for (var startIdx in data.map) {
-            // Maintain the indexMap so that we can implement indexOf(record)
-            for (var i = 0, len = data.map[startIdx].value.length; i < len; i++) {
-                data.indexMap[data.map[startIdx].value[i].internalId] = storeIndex++;
-            }
-        }
-
-        return record;
-    },
-
-
-    privates : {
-
-        isFirstPageLoaded : function() {
-            return !!this.getData().map[1]
-        },
-
-        cmpFuncHelper : function(val1, val2) {
-
-            return val1 < val2
-                   ? -1
-                   : val1 === val2
-                     ? 0
-                     : 1;
-        },
-
-
-        /**
-         * Same values: Presedence is given the newly inserted record.
-         *
-         * @param record
-         * @returns {*}
-         */
-        findInsertIndexInPagesForRecord : function(record) {
-
-            var me  = this,
-                map = me.getData().map,
-                // guaranteed to be only one sorter
-                sorters     = me.getSorters(),
-                target      = null,
-                isBeginning = this.isFirstPageLoaded(),
-                pageIterate,
-                values, property,
-                direction, cmpRecord, cmp, cmpFunc,
-                firstPage;
-
-            // iterate through maps
-            // insert at map n
-            // shift records through pages
-            // update indexMap
-            if (!sorters || sorters.length != 1) {
-                return [1, 0];
-            }
-
-            property  = sorters.getAt(0).getProperty();
-            direction = sorters.getAt(0).getDirection();
-
-            cmpFunc = record.getField(property)
-                      ? record.getField(property).compare
-                      : me.cmpFuncHelper;
-
-            for (var i in map) {
-
-                if (!map.hasOwnProperty(i)) {
-                    continue;
-                }
-
-                pageIterate = parseInt(i, 10) ;
-                firstPage   = firstPage ? firstPage : pageIterate;
-
-                values = map[i].value;
-
-                for (var a = 0, lena = values.length; a < lena; a++) {
-                    cmpRecord = values[a];
-
-                    cmp = cmpFunc(record.get(property), cmpRecord.get(property));
-
-                    // -1 less, 0 equal, 1 greater
-                    switch (direction) {
-                        case 'ASC':
-                            if (cmp === 0) {
-                                return [pageIterate, a];
-                            } else if (cmp === -1) {
-
-                                if (a === 0) {
-                                    if (firstPage === 1 && pageIterate >= 1) {
-                                        return [pageIterate, a];
-                                    } else {
-                                        return null;
-                                    }
-                                }
-
-                                return [pageIterate, a];
-                            }
-                            break;
-                        default:
-                            //console.log(record.get(property), cmpRecord.get(property), cmp);
-                            if (record.get(property) === 10000.5) {
-                                console.log(record.get(property), cmpRecord.get(property), cmp, a, pageIterate);
-                            }
-                           // console.log(record.get(property), cmpRecord.get(property), cmp);
-                            if (cmp === 0) {
-                                return [pageIterate, a];
-                            } else if (cmp === 1) {
-                                if (firstPage !== 1 &&  !map.hasOwnProperty(pageIterate - 1)) {
-                                    return null;
-                                }
-                                return [pageIterate, a];
-                            }
-                            break;
-                    }
-                }
-            }
-
-            return null;
-
-        }
-
     }
+
+
 
 });
