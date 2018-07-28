840e7b44976287f3a4bdfdc309238ffde4d76ee7
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2018-07-28 09:19:43 +0200

    - enhancement: narrowed random computing of date down to also consider current date (see conjoon/app-cn_mail#34)

--- src/data/mail/ajax/sim/message/MessageTable.js
+++ src/data/mail/ajax/sim/message/MessageTable.js
@@ -56,9 +56,10 @@ Ext.define('conjoon.cn_mail.data.mail.ajax.sim.message.MessageTable', {
 
     buildRandomDate : function() {
         var me = this,
-            d  = me.buildRandomNumber(1, 31),
-            m  = me.buildRandomNumber(1, 12),
-            y  = me.buildRandomNumber(2007, 2017),
+            dt  = new Date(),
+            y  = me.buildRandomNumber(2007, dt.getFullYear()),
+            m  = me.buildRandomNumber(1, y == dt.getFullYear() ? dt.getMonth() : 12),
+            d  = me.buildRandomNumber(1, y == dt.getFullYear() && m == dt.getMonth() ? dt.getDate() : 31),
             h  = me.buildRandomNumber(0, 23),
             i  = me.buildRandomNumber(0, 59),
             pad = function(v) {
