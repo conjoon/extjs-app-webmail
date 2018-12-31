0767095b7476981286ca75693fe0d9ba157ddd5e
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2018-12-31 10:29:57 +0100

    - enhancement: removed account informations from MailFolderSim (see conjoon/app-cn_mail#91)

--- src/data/mail/ajax/sim/folder/MailFolderSim.js
+++ src/data/mail/ajax/sim/folder/MailFolderSim.js
@@ -32,105 +32,86 @@ Ext.define('conjoon.cn_mail.data.mail.ajax.sim.folder.MailFolderSim', {
 
 }, function() {
 
-    var mailFolders =   [{
-        id            : "dev_sys_conjoon_org",
-        text          : "dev@conjoon.org",
+    var folders =   [{
+        id            : "INBOX",
+        text          : 'Inbox',
+        unreadCount   : 3787,
+        type          : 'INBOX',
+        data      : [{
+            id            : "INBOX.MyStuff",
+            text          : 'MyStuff',
+            unreadCount   : 3787,
+            type          : 'INBOX',
+            data      : [],
+            mailAccountId :  "dev_sys_conjoon_org",
+        }],
         mailAccountId :  "dev_sys_conjoon_org",
-        type          : 'ACCOUNT',
-        expanded      : true,
-        from          : 'dev@conjoon.org',
-        replyTo       : 'dev@conjoon.org',
-        name          : "conjoon developer",
-        children : [{
-            id            : "INBOX",
-            text          : 'Inbox',
-            unreadCount   : 3787,
-            type          : 'INBOX',
-            children      : [{
-                id            : "INBOX.ToDo",
-                text          : 'ToDo',
-                unreadCount   : 3787,
-                expandedn     : true,
-                type          : 'INBOX',
-                children      : [],
-                mailAccountId :  "dev_sys_conjoon_org",
-            }],
-            mailAccountId :  "dev_sys_conjoon_org"
-        }, {
-            id            : "INBOX.Sent Messages",
-            text          : 'Sent',
-            unreadCount   : 0,
-            type          : 'SENT',
-            children      : [],
-            mailAccountId :  "dev_sys_conjoon_org",
-        }, {
-            id            : "INBOX.Junk",
-            text          : 'Junk',
-            unreadCount   : 0,
-            type          : 'JUNK',
-            children      : [],
-            mailAccountId :  "dev_sys_conjoon_org",
-        }, {
-            id            : "INBOX.Drafts",
-            text          : 'Drafts',
-            unreadCount   : 0,
-            type          : 'DRAFT',
-            children      : [],
-            mailAccountId :  "dev_sys_conjoon_org",
-        }, {
-            id           : "INBOX.Trash",
-            text         : 'Trash',
-            unreadCount  : 5,
-            type         : 'TRASH',
-            children     : [],
-            mailAccountId :  "dev_sys_conjoon_org",
-        }]
     }, {
-        id            : "mail_account",
-        text          : "demo@googlemail.com",
+        id            : "INBOX.Sent Messages",
+        text          : 'Sent',
+        unreadCount   : 0,
+        type          : 'SENT',
+        data      : [],
+        mailAccountId :  "dev_sys_conjoon_org",
+    }, {
+        id            : "INBOX.Junk",
+        text          : 'Junk',
+        unreadCount   : 0,
+        type          : 'JUNK',
+        data      : [],
+        mailAccountId :  "dev_sys_conjoon_org",
+    }, {
+        id            : "INBOX.Drafts",
+        text          : 'Drafts',
+        unreadCount   : 0,
+        type          : 'DRAFT',
+        data      : [],
+        mailAccountId :  "dev_sys_conjoon_org",
+    }, {
+        id           : "INBOX.Trash",
+        text         : 'Trash',
+        unreadCount  : 5,
+        type         : 'TRASH',
+        data     : [],
+        mailAccountId :  "dev_sys_conjoon_org",
+    },
+    //////////////////////////////
+    {
+        id            : "INBOX",
+        text          : 'Inbox',
+        unreadCount   : 3787,
+        type          : 'INBOX',
+        data      : [],
         mailAccountId :  "mail_account",
-        type          : 'ACCOUNT',
-        expanded      : true,
-        from          : 'demo@googlemail.com',
-        replyTo       : 'demo@googlemail.com',
-        name          : "google mail",
-        children : [{
-            id            : "INBOX",
-            text          : 'Inbox',
-            unreadCount   : 3787,
-            type          : 'INBOX',
-            children      : [],
-            mailAccountId :  "mail_account",
 
-        }, {
-            id            : "INBOX.Sent Messages",
-            text          : 'Sent',
-            unreadCount   : 0,
-            type          : 'SENT',
-            children      : [],
-            mailAccountId :  "mail_account",
-        }, {
-            id            : "INBOX.Junk",
-            text          : 'Junk',
-            unreadCount   : 0,
-            type          : 'JUNK',
-            children      : [],
-            mailAccountId :  "mail_account",
-        }, {
-            id            : "INBOX.Drafts",
-            text          : 'Drafts',
-            unreadCount   : 0,
-            type          : 'DRAFT',
-            children      : [],
-            mailAccountId :  "mail_account",
-        }, {
-            id           : "INBOX.Trash",
-            text         : 'Trash',
-            unreadCount  : 5,
-            type         : 'TRASH',
-            children     : [],
-            mailAccountId :  "mail_account",
-        }]
+    }, {
+        id            : "INBOX.Sent Messages",
+        text          : 'Sent',
+        unreadCount   : 0,
+        type          : 'SENT',
+            data      : [],
+        mailAccountId :  "mail_account",
+    }, {
+        id            : "INBOX.Junk",
+        text          : 'Junk',
+        unreadCount   : 0,
+        type          : 'JUNK',
+            data      : [],
+        mailAccountId :  "mail_account",
+    }, {
+        id            : "INBOX.Drafts",
+        text          : 'Drafts',
+        unreadCount   : 0,
+        type          : 'DRAFT',
+            data      : [],
+        mailAccountId :  "mail_account",
+    }, {
+        id           : "INBOX.Trash",
+        text         : 'Trash',
+        unreadCount  : 5,
+        type         : 'TRASH',
+            data     : [],
+        mailAccountId :  "mail_account"
     }];
 
 
@@ -138,20 +119,34 @@ Ext.define('conjoon.cn_mail.data.mail.ajax.sim.folder.MailFolderSim', {
     Ext.ux.ajax.SimManager.register({
         type : 'json',
 
-        url  : /cn_mail\/MailFolder(\/\d+)?/,
+        url : /cn_mail\/MailAccounts\/(.+)\/MailFolders(\/.*)?/im,
 
-        data: function(ctx) {
+        doGet: function(ctx) {
+            const me            = this,
+                  mailAccountId = ctx.params.mailAccountId;
 
-            var idPart = ctx.url.match(this.url)[1],
-                id;
-            if (idPart) {
-                id = parseInt(idPart.substring(1), 10);
-                return Ext.Array.findBy(mailFolders, function(mailFolder) {
-                    return mailFolder.id === id;
-                });
-            } else {
-                return mailFolders;
-            }
+            ret = {};
+
+            let mailFolders = Ext.Array.filter(
+                folders,
+                function(item) {
+                    return item.mailAccountId === '' + mailAccountId;
+                }
+            );
+
+
+            ret.responseText = Ext.JSON.encode({
+                data :  mailFolders
+            });
+
+            Ext.Array.forEach(me.responseProps, function (prop) {
+                if (prop in me) {
+                    ret[prop] = me[prop];
+                }
+            });
+            return ret;
+
+
         }
     });
 
