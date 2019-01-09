643c065bad208dfee7f6840eae18bb0b0d3350f9
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2019-01-09 17:26:29 +0100

    - enhancement: removed obsolete code (see conjoon/app-cn_mail#91)

--- src/view/mail/inbox/InboxViewModel.js
+++ src/view/mail/inbox/InboxViewModel.js
@@ -41,10 +41,6 @@ Ext.define('conjoon.cn_mail.view.mail.inbox.InboxViewModel', {
     alias : 'viewmodel.cn_mail-mailinboxviewmodel',
 
     stores : {
-       /* 'cn_mail-mailfoldertreestore' : {
-            type     : 'cn_mail-mailfoldertreestore',
-            autoLoad : true
-        },*/
         'cn_mail-mailmessageitemstore' : {
             type     : 'cn_mail-mailmessageitemstore',
             autoLoad : true,
