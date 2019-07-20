e8b4679eaede912ed2dd24f7498de13f1dcce028
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2019-07-20 11:07:39 +0200

    - enhancement: updated default css; made sure content-security-policy is set and provides appropriate value; added "imagesAllowed" property to indicate whether it's okay for this iframe to load external images

--- classic/src/view/mail/message/reader/MessageViewIframe.js
+++ classic/src/view/mail/message/reader/MessageViewIframe.js
@@ -34,36 +34,111 @@ Ext.define('conjoon.cn_mail.view.mail.message.reader.MessageViewIframe', {
 
     alias : 'widget.cn_mail-mailmessagereadermessageviewiframe',
 
+
+    publishes : [
+        /**
+         * Property to indicate whether this Iframe allows loading of
+         * External images.
+         * This value is set and published by calling setSrcDoc with a
+         * second argument, defaulting to the value of this class'
+         * property "imagesAllowed".
+         *
+         * @type {Boolean} imagesAllowed
+         */
+        "imagesAllowed"
+    ],
+
+    /**
+     * Whether this iframe allows loading of external resources.
+     * Set with setSrcDoc
+     *
+     * @type {Boolean} imagesAllowed
+     *
+     * @private
+     *
+     * @see setSrcDoc
+     */
+    imagesAllowed : false,
+
+
     /**
      * Overridden to make sure the return value of #getDefaultBodyCss is
-     * prepended to the value that gets set.
+     * prepended to the value that gets set, along with the "Content-Security-Policy"
+     * meta-tag.
      * Will not prepend the value if an empty string or a falsy value was
      * submitted.
+     * Additionally, the "imagesAllowed"-property can be passed with indicates
+     * whether images are allowed in the value passed to the iframe's src doc.
+     * The state of this property will be published with the "imagesAllowed" property.
+     *
+     * @param {String} value
+     * @param {Boolean} imagesAllowed
+     *
+     * @return {String}
      *
      * @inheritdoc
+     *
+     * @see getContentSecurityPolicy
+     * @see getDefaultCss
      */
-    setSrcDoc : function(value) {
+    setSrcDoc : function(value, imagesAllowed = false) {
 
-        const me = this;
+        const me = this,
+              head = [];
 
-        value = value ? me.getDefaultCss() + value : value;
+        me.imagesAllowed = !!imagesAllowed;
+
+        me.publishState("imagesAllowed", me.imagesAllowed);
+
+        head.push(me.getContentSecurityPolicy(me.imagesAllowed), me.getDefaultCss());
+
+        value = value ? head.join("") + value : value;
 
         return me.callParent([value]);
     },
 
 
+    /**
+     * Returns the meta tag with the http-equiv "Content-Security-Policy" defaulting to
+     * content="default-src 'none'; script-src 'self'; connect-src 'self'; img-src data: 'self'; style-src * 'unsafe-inline';"
+     * If the argument imagesAllowed is set to true, the img-src is set to "data: *" to make sure
+     * external image loading is allowed.
+     *
+     * @param {Boolean} withImages
+     *
+     *
+     * @returns {string}
+     */
+    getContentSecurityPolicy : function(imagesAllowed = false) {
+
+        if (imagesAllowed === true) {
+            return "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'none'; script-src 'self'; connect-src 'self'; img-src data: *; style-src * 'unsafe-inline';\">";
+
+        }
+        return "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'none'; script-src 'self'; connect-src 'self'; img-src data: 'self'; style-src * 'unsafe-inline';\">";
+
+    },
+
+
+    /**
+     * Returns a default css to style the content of this iframe
+     *
+     * @returns {string}
+     */
     getDefaultCss : function() {
 
-        return ["<style type=\"text/css\">",
+        return [
+            "<style type=\"text/css\">",
             " html, body {",
-            " height:-moz-max-content;",
-            " width:-moz-max-content;",
-            " height:max-content;",
-            " width:max-content;",
             "margin:0px;",
             "padding:0px;",
             "}",
+            "p {margin:0px; margin-block-start:0px;margin-block-end: 0px;}",
             "body {",
+            "min-height:fit-content;;",
+            "min-width:fit-content;",
+            "width:100%;",
+            "height:100%;",
             "text-align:justify;",
             "color: rgb(64, 64, 64);",
             " font-family: \"Open Sans\", \"Helvetica Neue\", helvetica, arial, verdana, sans-serif;",
@@ -77,9 +152,17 @@ Ext.define('conjoon.cn_mail.view.mail.message.reader.MessageViewIframe', {
             "border-left:4px solid #bee9fc;",
             "}",
             "</style>"].join('');
+    },
+
+
+    /**
+     * Returns this instance's value of imagesAllowed
+     *
+     * @param {Boolen} value
+     */
+    getImagesAllowed : function() {
+        return this.imagesAllowed;
     }
-
-
 
 
 });
--- tests/classic/src/view/mail/message/reader/MessageViewIframeTest.js
+++ tests/classic/src/view/mail/message/reader/MessageViewIframeTest.js
@@ -26,17 +26,40 @@
 describe('conjoon.cn_mail.view.mail.message.reader.MessageViewIframeTest', function(t) {
 
 
-    t.it("config / default checks / setSrcDoc()", function (t) {
-        let iframe = Ext.create('conjoon.cn_mail.view.mail.message.reader.MessageViewIframe', {
-            renderTo : document.body
-        });
+    let iframe;
 
+    const META = {
+            noimg : "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'none'; script-src 'self'; connect-src 'self'; img-src data: 'self'; style-src * 'unsafe-inline';\">",
+            img : "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'none'; script-src 'self'; connect-src 'self'; img-src data: *; style-src * 'unsafe-inline';\">"
+        },
+        buildIframe = function() {
+            iframe = Ext.create('conjoon.cn_mail.view.mail.message.reader.MessageViewIframe', {
+                renderTo : document.body
+            });
+        };
+
+
+
+    t.afterEach(function() {
+
+        if (iframe) {
+            iframe.destroy();
+            iframe = null;
+        }
+
+    })
+
+
+    t.it("config / default checks / setSrcDoc()", function (t) {
+        buildIframe();
 
         t.isInstanceOf(iframe, 'coon.comp.component.Iframe');
 
         t.expect(iframe.alias).toContain('widget.cn_mail-mailmessagereadermessageviewiframe');
 
         let str = iframe.getDefaultCss();
+
+        let policy = iframe.getContentSecurityPolicy();
 
         t.expect(Ext.isString(str)).toBe(true);
         t.expect(str.length).toBeGreaterThan(1);
@@ -45,15 +68,73 @@ describe('conjoon.cn_mail.view.mail.message.reader.MessageViewIframeTest', funct
 
         iframe.setSrcDoc(val);
 
-        t.expect(iframe.getSrcDoc()).toBe(str + val);
+        t.expect(iframe.getSrcDoc()).toBe(policy + str + val);
 
         iframe.setSrcDoc(null);
 
         t.expect(iframe.getSrcDoc()).toBe("");
 
+        t.expect(iframe.getImagesAllowed()).toBe(false);
 
-        iframe.destroy();
-        iframe = null;
+        t.expect(iframe.getPublishes().imagesAllowed).toBe(true);
+
     });
+
+
+    t.it("getContentSecurityPolicy()", function(t) {
+
+        buildIframe();
+
+        t.expect(iframe.getContentSecurityPolicy()).toBe(META.noimg);
+
+        t.expect(iframe.getContentSecurityPolicy(true)).toBe(META.img);
+    });
+
+
+    t.it("getImagesAllowed", function(t) {
+
+        buildIframe();
+
+        t.expect(iframe.getImagesAllowed()).toBe(false);
+        iframe.imagesAllowed = true;
+        t.expect(iframe.getImagesAllowed()).toBe(true);
+
+    });
+
+
+    t.it("setSrcDoc()", function(t) {
+
+        buildIframe();
+
+        let IMAGESALLOWED = null;
+
+        let NOEXTERNALMETA = null;
+
+        iframe.getContentSecurityPolicy = function(allowed) {
+            EXTERNALMETA = allowed;
+        };
+
+        iframe.publishState = function(key, val) {
+            if (key === "imagesAllowed") {
+                IMAGESALLOWED = val;
+            }
+        }
+
+        iframe.setSrcDoc("foo");
+        t.expect(iframe.getImagesAllowed()).toBe(false);
+        t.expect(IMAGESALLOWED).toBe(false);
+        t.expect(EXTERNALMETA).toBe(false);
+
+        iframe.setSrcDoc("foo", true);
+        t.expect(iframe.getImagesAllowed()).toBe(true);
+        t.expect(IMAGESALLOWED).toBe(true)
+        t.expect(EXTERNALMETA).toBe(true);
+
+    });
+
+
+
+
+
 
 });
