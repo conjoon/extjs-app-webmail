801da1098e65813b51260252a43101ff16533e4b
Thorsten Suckow-Homberg <thorsten@sucko-homberg.de>
2022-11-14 11:39:07 +0100

    feat: add protocol handler for mailto
    refs conjoon/extjs-app-webmail#250

---
+++ src/app/plugin/MailtoProtocolHandlerPlugin.js
@@ -0,0 +1,51 @@
+/**
+ * coon.js
+ * extjs-app-webmail
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/extjs-app-webmail
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * ControllerPlugin for registering mailto-links with extjs-app-webmail.
+ *
+ */
+Ext.define("conjoon.cn_mail.app.plugin.MailtoProtocolHandlerPlugin", {
+
+    extend: "coon.core.app.plugin.ControllerPlugin",
+
+    /**
+     * @inheritdoc
+     */
+    run (controller) {
+        "use strict";
+
+        const origin = window.location.origin;
+
+        navigator.registerProtocolHandler(
+            "mailto",
+            origin + "/#cn_mail/message/compose/%s",
+            "mailto handler"
+        );
+
+        return true;
+    }
+
+});
