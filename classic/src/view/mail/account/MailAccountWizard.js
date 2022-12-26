7d817a5c6d15a0b6ca6ac877b37895e3a72a13bd
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2022-12-26 12:50:34 +0100

    refactor: update setBusy() to accept custom msg
    refs conjoon/conjoon#23

--- classic/src/view/mail/account/MailAccountWizard.js
+++ classic/src/view/mail/account/MailAccountWizard.js
@@ -188,7 +188,7 @@ Ext.define("conjoon.cn_mail.view.mail.account.MailAccountWizard", {
     ],
 
 
-    setBusy (show = true) {
+    setBusy (show = true, msg = "Loading configuration") {
         const me = this;
 
         let mask = me.busyMask;
@@ -205,7 +205,7 @@ Ext.define("conjoon.cn_mail.view.mail.account.MailAccountWizard", {
                 /**
                  * @i18n
                  */
-                msg: "Loading configuration",
+                msg,
                 msgAction: "Please wait...",
                 glyphCls: "fa fa-spin fa-gear",
                 target: me
@@ -214,6 +214,7 @@ Ext.define("conjoon.cn_mail.view.mail.account.MailAccountWizard", {
         }
 
         mask.show();
+        mask.updateMsg(msg);
         mask.loopProgress();
         return mask;
     }
