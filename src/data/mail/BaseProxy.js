c3f37bfef0f50f4d96b4c997a2c8a1d1e9a975fc
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-01-06 19:23:59 +0100

    fix: filter for MessageEntityProxy cannot be purged
    move encodeFilters() method specific to filters in polish notation
    to MailAccountProxy
    
    refs conjoon/conjoon#23

--- src/data/mail/BaseProxy.js
+++ src/data/mail/BaseProxy.js
@@ -91,25 +91,6 @@ Ext.define("conjoon.cn_mail.data.mail.BaseProxy", {
         };
 
         return actionMethods[request.getAction()];
-    },
-
-
-    /**
-     * Overriden to consider filter syntax according to
-     * https://www.conjoon.org/docs/api/rest-api/@conjoon/rest-api-description/rest-api-email
-     *
-     * @return {String}
-     */
-    encodeFilters (filters) {
-
-        const me = this;
-
-        if (!me.filterEncoder) {
-            me.filterEncoder = Ext.create("conjoon.cn_mail.data.jsonApi.PnFilterEncoder");
-        }
-
-        return me.filterEncoder.encode(filters);
     }
-
 
 });
