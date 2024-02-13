870e0f296b8423c36714b120f91c6afb1329cdea
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-13 12:13:47 +0100

    refactor: do not encode as conjunction if only one filter specified

--- tests/src/_issues_/refactor/php-lib-conjoon#8.js
+++ tests/src/_issues_/refactor/php-lib-conjoon#8.js
@@ -1,26 +1,10 @@
 /**
- * conjoon
- * extjs-app-webmail
- * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/extjs-app-webmail
+ * This file is part of the conjoon/extjs-app-webmail project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2022-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 StartTest(t => {
@@ -197,4 +181,26 @@ StartTest(t => {
         ].map(proxy => testWithProxy(proxy));
     });
 
+
+    t.it("no AND conjunction in single Filter param",  t => {
+
+        const encoder = Ext.create("conjoon.cn_mail.data.jsonApi.PnFilterEncoder");
+        const filterCollection = Ext.create("Ext.util.FilterCollection");
+
+        filterCollection.add(Ext.create("Ext.util.Filter", {
+            property: "date",
+            operator: ">=",
+            value: 1000
+        }));
+
+        const serializeSpy = t.spyOn(conjoon.cn_mail.data.jsonApi.PnFilterEncoder.prototype, "serialize").and.callThrough();
+
+        t.expect(encoder.encode(filterCollection)).toBe(
+            JSON.stringify({
+                ">=": {date: 1000}
+            })
+        );
+
+        serializeSpy.remove();
+    });
 });
